f6d13d8229e131c348689190e62781c994184e30
DzhumagazievaDiana <113889448+DzhumagazievaDiana@users.noreply.github.com>
2023-04-21 11:41:58 +0300

    Add files via upload

---
+++ ClothesCollection/ConsoleApp17/ClothesRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesCollection
+{
+    public class ClothesRepository
+    {
+        private List<Clothes> _clothes;
+
+        public ClothesRepository()
+        {
+            _clothes = new List<Clothes>();
+        }
+
+        public List<Clothes> GetAll()
+        {
+            return _clothes;
+        }
+
+        public Clothes GetById(int id)
+        {
+            return _clothes.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Insert(Clothes clothes)
+        {
+            clothes.Id = _clothes.Count + 1;
+            _clothes.Add(clothes);
+        }
+
+        public void Delete(int id)
+        {
+            var clothes = GetById(id);
+            if (clothes != null)
+            {
+                _clothes.Remove(clothes);
+            }
+        }
+
+        public void Update(Clothes clothes)
+        {
+            var existingClothes = GetById(clothes.Id);
+            if (existingClothes != null)
+            {
+                existingClothes.Brand = clothes.Brand;
+                existingClothes.Style = clothes.Style;
+                existingClothes.Price = clothes.Price;
+            }
+        }
+
+        public List<Clothes> Find(Func<Clothes, bool> predicate)
+        {
+            return _clothes.Where(predicate).ToList();
+        }
+    }
+}
+    
+
---
+++ ClothesCollection/ConsoleApp17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClothesCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new ClothesRepository();
+
+            repository.Insert(new Clothes { Brand = "Dior", Style = "Casual", Price = 50.99m });
+            repository.Insert(new Clothes { Brand = "Channel", Style = "Formal", Price = 35.50m });
+            repository.Insert(new Clothes { Brand = "Gucci", Style = "Luxury", Price = 500.00m });
+
+            var allClothes = repository.GetAll();
+            Console.WriteLine("All clothes:");
+
+            foreach (var clothes in allClothes)
+            {
+                Console.WriteLine($"Id: {clothes.Id}, Brand: {clothes.Brand}, Style: {clothes.Style}, Price: {clothes.Price}");
+            }
+
+            var clothesById = repository.GetById(2);
+            Console.WriteLine("\nClothes by id:");
+            Console.WriteLine($"Id: {clothesById.Id}, Brand: {clothesById.Brand}, Style: {clothesById.Style}, Price: {clothesById.Price}");
+
+            var expensiveClothes = repository.Find(c => c.Price > 100);
+            Console.WriteLine("\nExpensive clothes:");
+            foreach (var clothes in expensiveClothes)
+            {
+                Console.WriteLine($"Id: {clothes.Id}, Brand: {clothes.Brand}, Style: {clothes.Style}, Price: {clothes.Price}");
+            }
+
+            repository.Update(new Clothes { Id = 1, Brand = "Mango", Style = "Casual", Price = 45.00m });
+            var updatedClothes = repository.GetById(1);
+            Console.WriteLine("\nUpdated clothes:");
+            Console.WriteLine($"Id: {updatedClothes.Id}, Brand: {updatedClothes.Brand}, Style: {updatedClothes.Style}, Price: {updatedClothes.Price}");
+
+            repository.Delete(3);
+            var deletedClothes = repository.GetById(3);
+            Console.WriteLine("\nDeleted clothes:");
+            Console.WriteLine(deletedClothes == null ? "Not found" : $"Id: {deletedClothes.Id}, Brand: {deletedClothes.Brand}, Style: {deletedClothes.Style}, Price: {deletedClothes.Price}");
+        }
+    }
+}
